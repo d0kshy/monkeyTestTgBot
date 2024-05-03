a956716fec3b9ff4a3ec1c73d8ce0d3ec742faa5
d0kshy <stdy.dk22@gmail.com>
2024-05-03 19:34:24 +0200

    add first question with buttons for the test

--- monkeyTest.py
+++ monkeyTest.py
@@ -3,20 +3,43 @@ from telebot import types
 import webbrowser
 import time
 
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot('7106621173:AAE7kIT1AQcDgRIHD94DyuKnPyYrQMCfznM')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    time.sleep(0.5)
-    markup = types.InlineKeyboardMarkup()
-    btn1 = types.InlineKeyboardButton('Start the test', callback_data='delete')
+    markup = types.ReplyKeyboardMarkup()
+    btn1 = types.KeyboardButton('Start the test')
     markup.row(btn1)
-    btn2 = types.InlineKeyboardButton('Author', url='https://google.com')
-    btn3 = types.InlineKeyboardButton('Donation', url='https://savelife.in.ua/')
+    btn2 = types.KeyboardButton('Author')
+    btn3 = types.KeyboardButton('Donation')
     markup.row(btn2, btn3)
-    btn4 = types.InlineKeyboardButton('Leave review', url='https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+    btn4 = types.KeyboardButton('Leave review')
     markup.row(btn4)
     bot.send_message(message.chat.id, f'Hi there, {message.from_user.first_name}! Choose the option:', reply_markup=markup)
+    bot.register_next_step_handler(message, on_click)
+
+def on_click(message):
+    if message.text == 'Start the test':
+        que1(message)
+    elif message.text == 'Author':
+        bot.send_message(message.chat.id, 'Welcome to my website!', parse_mode='html')
+        time.sleep(1.0)
+        webbrowser.open('https://google.com')
+        bot.register_next_step_handler(message, on_click)
+    elif message.text == 'Donation':
+        webbrowser.open('https://savelife.in.ua/en')
+        bot.register_next_step_handler(message, on_click)
+    elif message.text == 'Leave review':
+        webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+        bot.register_next_step_handler(message, on_click)
+
+def que1(message):
+    markup = types.InlineKeyboardMarkup()
+    btn2 = types.InlineKeyboardButton('Pink', callback_data='color_pink')
+    btn3 = types.InlineKeyboardButton('Yellow', callback_data='color_yellow')
+    markup.row(btn2, btn3)
+    bot.send_message(message.chat.id, f'What color would you prefer?', reply_markup=markup)
+
 
 @bot.message_handler(commands=['author'])
 def donation(message):
@@ -28,7 +51,7 @@ def donation(message):
 def donation(message):
     bot.send_message(message.chat.id, 'Thank you for supporting Ukraine! 🇺🇦', parse_mode='html')
     time.sleep(1.0)
-    webbrowser.open(url='https://savelife.in.ua/')
+    webbrowser.open(url='https://savelife.in.ua/en/')
 
 @bot.message_handler(commands=['review'])
 def donation(message):
