e0b4c10b2c66f2ce91d03ed6bab25123f4f67a9f
d0kshy <stdy.dk22@gmail.com>
2024-05-22 11:03:16 +0200

    Fixed the test

--- monkeyTest.py
+++ monkeyTest.py
@@ -3,20 +3,20 @@ from telebot import types
 import webbrowser
 import time
 
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot('7106621173:AAE7kIT1AQcDgRIHD94DyuKnPyYrQMCfznM')
 
 user_data = {}
 
 questions = [
     "What color do you prefer?",
-    "Are you introvert or extrovert?"
+    "Are you an introvert or extrovert?"
 ]
 
 monkeyType = [
-    {"name": "Such a cool monkey with a cool yellow hawaiian shirt!", "color": "Yellow", "psychtype": "Extravert"},
+    {"name": "Such a cool monkey with a cool yellow Hawaiian shirt!", "color": "Yellow", "psychtype": "Extrovert"},
     {"name": "Such a sleepy monkey with a cute yellow blanket!", "color": "Yellow", "psychtype": "Introvert"},
-    {"name": "Such a cool monkey with a nice pink dress!", "color": "Pink", "psychtype": "Extravert"},
-    {"name": "Such a sleepy monkey with a cool pink pijama!", "color": "Pink", "psychtype": "Introvert"}
+    {"name": "Such a cool monkey with a nice pink dress!", "color": "Pink", "psychtype": "Extrovert"},
+    {"name": "Such a sleepy monkey with a cool pink pajama!", "color": "Pink", "psychtype": "Introvert"}
 ]
 
 @bot.message_handler(commands=['start'])
@@ -36,54 +36,72 @@ def start(message):
 def on_click(message):
     if message.text == 'Start the test':
         bot.send_message(message.chat.id, 'Let\'s start the test', parse_mode='html')
-        bot.register_next_step_handler(message, handle_answers)
+        user_data[message.chat.id] = {}
         send_question(message.chat.id, 0)
     elif message.text == 'Author':
         bot.send_message(message.chat.id, 'Welcome to my website!', parse_mode='html')
         time.sleep(1.0)
         webbrowser.open('https://google.com')
-        bot.register_next_step_handler(message, on_click)
     elif message.text == 'Donation':
         webbrowser.open('https://savelife.in.ua/en')
-        bot.register_next_step_handler(message, on_click)
     elif message.text == 'Leave review':
         webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-        bot.register_next_step_handler(message, on_click)
 
 def send_question(chat_id, question_index):
-    question = questions[question_index]
-    
-    bot.send_message(chat_id, question)
-    user_data[chat_id]['question_index'] = question_index
-
-
-@bot.message_handler(func=lambda message: message.chat.id in user_data)
-def handle_answers(message):
-    chat_id = message.chat.id
-    answer = message.text.lower()
-    question_index = user_data[chat_id].get('question_index', 0)
-
-    if question_index == 0:
-        user_data[chat_id]['color'] = answer
-    elif question_index == 1:
-        user_data[chat_id]['psychtype'] = answer
-
-    question_index += 1
     if question_index < len(questions):
-        send_question(chat_id, question_index)
+        question = questions[question_index]
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        
+        if question_index == 0:
+            btn1 = types.KeyboardButton('Yellow')
+            btn2 = types.KeyboardButton('Pink')
+            markup.row(btn1, btn2)
+        elif question_index == 1:
+            btn1 = types.KeyboardButton('Introvert')
+            btn2 = types.KeyboardButton('Extrovert')
+            markup.row(btn1, btn2)
+        
+        bot.send_message(chat_id, question, reply_markup=markup)
+        bot.register_next_step_handler_by_chat_id(chat_id, lambda msg: handle_answers(msg, question_index))
     else:
-        selected_monkey = select_monkey(user_data[chat_id])
-        bot.send_message(chat_id, f"Here is your result: {selected_monkey['name']}", reply_markup=types.ReplyKeyboardRemove())
-        user_data.pop(chat_id)   
+        send_result(chat_id)
 
+def handle_answers(message, question_index):
+    chat_id = message.chat.id
+    if chat_id not in user_data:
+        user_data[chat_id] = {}
+    
+    if question_index == 0:
+        user_data[chat_id]['color'] = message.text
+    elif question_index == 1:
+        user_data[chat_id]['psychtype'] = message.text
+    
+    send_question(chat_id, question_index + 1)
 
-def select_monkey(user_data):
-    for monkey in monkeyType:
-        if (user_data['color'] == monkey['color'] and 
-            user_data['psychType'] == monkey['psychType']):
-            return monkey
-    return {"name": "No monkey type found"}
- 
+def send_result(chat_id):
+    user_answers = user_data.get(chat_id, {})
+    color = user_answers.get('color')
+    psychtype = user_answers.get('psychtype')
+
+    result = next((monkey for monkey in monkeyType if monkey['color'] == color and monkey['psychtype'] == psychtype), None)
+    
+    if result:
+        bot.send_message(chat_id, result['name'])
+    else:
+        bot.send_message(chat_id, 'Sorry, we could not find a match for your preferences.')
+
+        # Show the main menu again after the test
+    time.sleep(0.5)
+    markup = types.ReplyKeyboardMarkup()
+    btn1 = types.KeyboardButton('Start the test')
+    markup.row(btn1)
+    btn2 = types.KeyboardButton('Author')
+    btn3 = types.KeyboardButton('Donation')
+    markup.row(btn2, btn3)
+    btn4 = types.KeyboardButton('Leave review')
+    markup.row(btn4)
+    bot.send_message(chat_id, 'What would you like to do next?', reply_markup=markup)
+    bot.register_next_step_handler_by_chat_id(chat_id, on_click)
 
 @bot.message_handler(commands=['author'])
 def author(message):
